3166cf057121ac74c4b1baa63dee6aafcafb6ad9
iespinoza96 <iespinoza@digis01.com>
2023-02-07 18:24:57 -0600

    Add project files.

---
+++ BL/Alumno.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Alumno
+    {
+        public static ML.Result Add(ML.Alumno alumno)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (var conn = new SqlConnection("Data Source=.;Initial Catalog=IEspinozaProgramacionNCapasGF2023;Persist Security Info=True;User ID=sa;Password=pass@word1"))
+                {
+                    var cmd = new SqlCommand("INSERT INTO [Alumno]([Nombre],[ApellidoPaterno],[ApellidoMaterno]) VALUES (@Nombre,@ApellidoPaterno,@ApellidoMaterno)", conn);
+                    cmd.Parameters.AddWithValue("@Nombre", alumno.Nombre);
+                    cmd.Parameters.AddWithValue("@ApellidoPaterno", alumno.ApellidoPaterno);
+                    cmd.Parameters.AddWithValue("@ApellidoMaterno", alumno.ApellidoMaterno);
+                    conn.Open();
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        result.Correct = true;
+                        result.Message = "Alumno insertado";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Ex = ex;
+                result.Correct = false;
+                result.Message = "Ocurrio un error al agregar el alumno";
+            }
+
+
+            return result;
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+    }
+}
---
+++ PL/Alumno.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Alumno
+    {
+        public static void Add()
+        {
+            ML.Alumno alumno = new ML.Alumno();
+
+            Console.WriteLine("Inserte el nombre del alumno: ");
+            alumno.Nombre = Console.ReadLine();
+
+            Console.WriteLine("Inserte el Apellido Paterno del alumno: ");
+            alumno.ApellidoPaterno = Console.ReadLine();
+            Console.WriteLine("Inserte el Apellido Materno del alumno: ");
+            alumno.ApellidoMaterno = Console.ReadLine();
+
+            
+            ML.Result result = BL.Alumno.Add(alumno);
+
+            if (result.Correct)
+            {
+                Console.WriteLine("Se inserto el alumno");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No se inserto el alumno");
+                Console.ReadKey();
+
+            }
+
+
+        }
+
+    }
+}
